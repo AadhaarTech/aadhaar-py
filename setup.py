2bdf32eda87e424dea451e21c60db4554ae1b2a2
Balamurali Pandranki <itsbalamurali@Balamuralis-MacBook-Pro.local>
2017-02-24 16:16:37 +0530

    Init repo with .gitignore , readme and lib setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'aadhaar',
+  packages = ['aadhaar'], 
+  version = '0.1',
+  description = 'Aadhaar API\'s',
+  author = 'Balamurali Pandranki',
+  author_email = 'murali@labwire.tech',
+  url = 'https://github.com/labwirehq/aadhaar-py',
+  download_url = 'https://github.com/labwirehq/aadhaar-py/tarball/0.1', 
+  keywords = ['aadhaar', 'iris', 'fingerscan','biometrics'],
+  classifiers = [],
+)
